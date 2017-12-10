f0bdb93c1d4b35176e37c1574d86e2b69f1d6b02
shenyuzhu <shenyuzhu@gomeplus.com>
2017-12-10 21:13:35 +0800

    spring profile maven

--- profile/src/test/java/com/tongdou/profile/ServiceTest.java
+++ profile/src/test/java/com/tongdou/profile/ServiceTest.java
@@ -4,21 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
-import static org.testng.Assert.*;
-
 /**
  * Created by shenyuzhu on 2017/12/9.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:ac-profile.xml")
-@ActiveProfiles("product")
-public class ProfileServiceTest {
+@ContextConfiguration(locations = "classpath:ac.xml")
+public class ServiceTest {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Resource
